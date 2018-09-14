68f84f2424879e9a628d6243e812b5773a831bc1
Pavlo Samsonov <samsonov.teamwork@gmail.com>
2018-09-14 22:21:40 +0300

    Remove ad sense tag from header, and place only on certain pages

--- divi-child/header.php
+++ divi-child/header.php
@@ -25,6 +25,25 @@
 	</script>
 
 	<?php wp_head(); ?>
+	
+<?php if( is_front_page() || is_singular( array('post', 'slide') ) || is_category() ) { ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125292761-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-125292761-1');
+</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-9355881364844265",
+    enable_page_level_ads: true
+  });
+</script>
+<?php } ?>
+
 </head>
 <body <?php body_class(); ?>>
 <?php
