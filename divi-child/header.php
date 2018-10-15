89b8a4e72cefe8339b87af540891270df4afa76b
Pavlo Samsonov <samsonov.teamwork@gmail.com>
2018-10-15 21:54:23 +0300

    Add hotjar tracking code

--- divi-child/header.php
+++ divi-child/header.php
@@ -78,6 +78,17 @@ h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
         width='0' height='0' style='display:none'/>
 </noscript>
 <?php } ?>
+<!-- Hotjar Tracking Code -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1047416,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 <body <?php body_class(); ?>>
 <?php
